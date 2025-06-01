ecf795e040900d36aa85ee92d6808c146d19b278
Ankit Raj <ankitrajcollab@gmail.com>
2025-06-01 12:29:18 +0530

    feat: complete Day 3 JavaScript exercises

---
+++ Day-03/index.js
@@ -0,0 +1,63 @@
+console.log("Day 03");
+
+// Operator - Symbols - that you apply on something to carr out a calculation.for different kind of operations you have different kind of operator .. so to perform an operaton you need a symbol that we call as operator .
+
+// Operands - these are the values on which your operator works on.
+
+// Expression - expression is something which resolve to a value .
+
+// x = 2 - you are assigning a value to x .
+
+// expression is something which can resolve to a value .
+
+// let x = 4 + 5; // this is expression - inside this expression too many things are happening .
+
+// use the operands and operator together to get another value as a output - this also called expression .
+
+// Expression can Resolve to a Value .
+
+// 3 + 4 - this is called evaluating expression
+
+//types of operator
+
+// Arithmetic
+// Assignment
+// Comparision
+// Relational
+// Logical
+// Bitwise
+// Conditional
+// Grouping
+// type of
+// instance of
+
+// Arithmatic Operator .
+
+let a = 10;
+let b = 20;
+
+console.log(a + b); // 30
+console.log(a - b); // -10
+console.log(b - a); // 10
+console.log(a * b); // 200
+console.log(a / b); // 0.5
+
+// String Concatination
+
+let f_name = 10;
+let l_name = 20;
+
+console.log(f_name + l_name);
+
+// exponential Operator (**)
+
+console.log(a ** b); // 10 t0 the power 20
+
+// Post Increment 
+
+// count++ it will return the value first then it increments thr value 
+
+// ++count it will increment the value then it will return. 
+
+// similarly there is post and pre decrement as well . 
+
