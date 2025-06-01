4aea978f7c25b32e3025aaeb39c047c2b75acb5f
Ankit Raj <ankitrajcollab@gmail.com>
2025-06-01 12:26:04 +0530

    feat: add initial setup and Day 1 JavaScript exercises

---
+++ Day-01/serverSide.js
@@ -0,0 +1,2 @@
+// here we are lloking how js is working in server side .
+console.log("hello from Node")
---
+++ Day-01/test.js
@@ -0,0 +1,5 @@
+console.log("here i have written test script");
+const docDiv = (document.getElementById("someId").innerText = "I am Some Div");
+// document.open();
+document.write("<h1>Hello World</h1>");
+// document.close();
